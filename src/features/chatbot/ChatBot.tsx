a749635f2994e842f048575f4def34f317accbb9
benjuwan <benjuwan.banquet12@gmail.com>
2026-03-27 23:14:25 +0900

    fix: zustandで設定したグローバルステートおよびアクションの設定.

--- src/features/chatbot/ChatBot.tsx
+++ src/features/chatbot/ChatBot.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import type { chatMessageType } from "./type/GeminiType";
+import { useChatviewStore } from "../../stores/useChatviewStore";
 import { ChatForm } from "./ChatForm";
 import { ChatSection } from "./ChatSection";
 
@@ -7,31 +8,31 @@ export const ChatBot = () => {
     const [loading, setLoading] = useState<boolean>(false);
     const [chatHistory, setChatHistory] = useState<chatMessageType[]>([]);
 
-    const [isChatView, setChatView] = useState<boolean>(false);
-    const handleChatView = (): void => {
-        setChatView(!isChatView);
-    }
+    const isChatView = useChatviewStore((state) => state.isChatView);
+    const handleChatView = useChatviewStore((state) => state.handleChatView);
 
     return (
         <>
-            <section className={`fixed w-full h-[95%] rounded-2xl bottom-0 z-9 grid items-end mt-[4em] pt-4 duration-250 ${isChatView ? 'isChatView shadow-[0px_0px_8px_rgba(0,0,0,.25)_inset] bg-[rgba(255,255,255,.85)] backdrop-blur-lg' : 'translate-y-[4em] opacity-0 invisible pointer-events-none'}`}>
-                <div className="p-4 pb-10 h-full overflow-y-auto overscroll-contain lg:flex lg:justify-around lg:items-start lg:flex-wrap lg:gap-[2%]">
-                    {(chatHistory.length > 0 || loading) &&
-                        <ChatSection props={{
+            {!isChatView && chatHistory.length > 0 ?
+                <button className="cursor-pointer fixed top-20 right-4 z-9 w-11 h-11 rounded-full grid place-content-center bg-[#eaeaea] shadow-[0px_0px_8px_rgba(0,0,0,.5)] hover:scale-[1.2] active:scale-[1.2] duration-250" type="button" onClick={handleChatView}>+</button> :
+                <section className={`fixed w-full h-[95%] rounded-2xl bottom-0 z-9 grid items-end mt-[4em] pt-4 duration-250 ${isChatView ? 'isChatView shadow-[0px_0px_8px_rgba(0,0,0,.25)_inset] bg-[rgba(255,255,255,.85)] backdrop-blur-lg' : 'translate-y-[4em] opacity-0 invisible pointer-events-none'}`}>
+                    <div className="p-4 pb-10 h-full overflow-y-auto overscroll-contain lg:flex lg:justify-around lg:items-start lg:flex-wrap lg:gap-[2%]">
+                        {(chatHistory.length > 0 || loading) &&
+                            <ChatSection props={{
+                                loading: loading,
+                                chatHistory: chatHistory
+                            }} />
+                        }
+                        <ChatForm props={{
                             loading: loading,
-                            chatHistory: chatHistory
+                            setLoading: setLoading,
+                            chatHistory: chatHistory,
+                            setChatHistory: setChatHistory,
+                            handleChatView: handleChatView
                         }} />
-                    }
-                    <ChatForm props={{
-                        loading: loading,
-                        setLoading: setLoading,
-                        chatHistory: chatHistory,
-                        setChatHistory: setChatHistory,
-                        handleChatView: handleChatView
-                    }} />
-                </div>
-            </section>
-            {isChatView || <button className="cursor-pointer fixed top-20 right-4 z-9 w-11 h-11 rounded-full grid place-content-center bg-[#eaeaea] shadow-[0px_0px_8px_rgba(0,0,0,.5)] hover:scale-[1.2] active:scale-[1.2] duration-250" type="button" onClick={handleChatView}>+</button>}
+                    </div>
+                </section>
+            }
         </>
     );
 };
